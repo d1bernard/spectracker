70c93a026e2ff8c5ed516890a2c81a20053690fc
dobernard <d1bernard@yahoo.fr>
2013-06-14 10:36:17 +0200

    initialisation du projet

---
+++ HTML5Application/public_html/js/plugins.js
@@ -0,0 +1,178 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+/*Création de larbre des requirements*/
+$('document').ready(function() {
+    /*initialisation de l'arbre*/
+    var rq_navtree=$("#rq_navtree");
+    //binding on move event
+    rq_navtree.bind("move_node.jstree",function(event,data){
+        alert(event);
+        alert(data.rslt.o.html());
+        alert(data.rslt.o.attr('id'));
+    });
+    rq_navtree.jstree({
+       "types": {
+         "valid_children":"all",
+         "types": {
+             "default": {
+                 "icon": {
+                            "image" : "img/bootstrap/glyphicons-halflings.png",
+                            "position": "-24px -24px"
+                         }
+             },
+             "project": {
+                        "icon": {
+                            "image" : "img/bootstrap/glyphicons-halflings.png",
+                            "position": "-48px -120px"
+                         }
+
+             },
+             "folder": {
+                        "icon": {
+                            "image" : "img/bootstrap/glyphicons-halflings.png",
+                            "position": "-384px -120px"
+                         }
+
+             },
+             "leaf": {
+                        "icon": {
+                            "image" : "img/bootstrap/glyphicons-halflings.png",
+                            "position": "-24px -24px"
+                         }
+             }
+         }
+       },
+               
+//        "json_data" : {
+//            "data" : [
+//                {
+//                    "data" : "A node",
+//                    "metadata" : { id : 23 },
+//                    "children" : [ "Child 1", "A Child 2" ]
+//                },
+//                {
+//                    "attr" : { "id" : "li.node.id1" },
+//                    "data" : {
+//                        "title" : "Long format demo",
+//                        "attr" : { "href" : "#" }
+//                    }
+//                }
+//            ]
+//        },
+        "json_data": {
+            "data": [{
+                "data" :{
+                    "title": "Project"
+                },
+                "attr": {
+                        "id": "project.requirement",
+                        "rel":"project"
+                },
+                
+                "children": [
+                        {
+                            "data" : {
+                                "title": "folder1"
+                            },
+                            "attr": {
+                                "id": "folder.1",
+                                "rel":"folder"
+                            },
+                            "children": [
+                                    {
+                                        "data" : {
+                                            "title": "folder2"
+                                        },
+                                        "attr": {
+                                            "id": "folder.2",
+                                            "rel":"folder"
+                                        },
+                                        "children": [
+                                                {
+                                                    "data" : {
+                                                        "title": "folder3"
+                                                    },
+                                                    "attr": {
+                                                        "id": "folder.3",
+                                                        "rel":"folder"
+                                                    }
+                                                },
+                                                {
+                                                    "data" : {
+                                                        "title": "document3"
+                                                    },
+                                                    "attr": {
+                                                        "id": "document.3",
+                                                        "rel":"leaf"
+                                                    }
+                                                }
+                                        ]
+
+                                    },
+                                    {
+                                        "data" : {
+                                            "title": "document2"
+                                        },
+                                        "attr": {
+                                            "id": "document.2",
+                                            "rel":"leaf"
+                                        }
+                                    }
+                            ]
+
+                        },
+                        {
+                            "data" : {
+                                "title": "document"
+                            },
+                            "attr": {
+                                "id": "document.1",
+                                "rel":"leaf"
+                            }
+                        }
+                ]
+
+            }]
+        },
+        'dnd' : {
+            'drop_finish' : function (){
+                alert("drop");
+            },
+            'drag_finish' : function (){
+                alert("drag");
+            }
+
+        },
+        "plugins": ["themes", "json_data", "crrm","types","dnd"]
+    });
+    
+    /*bind sur l'ouverture de noeud*/
+//    $("#rq_navtree").bind("open_node.jstree", function (event, data) { 
+////        if((data.inst._get_parent(data.rslt.obj)).length) { 
+////            data.inst._get_parent(data.rslt.obj).open_node(this, false); 
+////        } 
+//        alert("toro");
+//    }); 
+    
+});
+// Place any jQuery/helper plugins in here.
